e2738dc21d071738215308f50dce53c82be57b27
Anastasia Honchar <honchar.anastasiia@chnu.edu.ua>
2026-03-18 22:35:28 +0200

    Add project files.

---
+++ Lab1/creational/abstract_factory/abstract_factory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.creational.abstract_factory
+{
+    internal class abstract_factory
+    {
+    }
+}
